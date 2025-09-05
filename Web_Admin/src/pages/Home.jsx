69dac15274db7c004eb7da6fcab50fdb90060819
Atharva <Atharvabandurkar8@gmail.com>
2025-09-05 18:05:25 +0530

    components added

--- Web_Admin/src/pages/Home.jsx
+++ Web_Admin/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import StatCard from "../components/StateCArd";
+import StatCard from "../components/StateCard";
 import Table from "../components/Tables"; // Correctly import the Tables component
 import { AlertCircle, CheckCircle, Clock, Flame } from "lucide-react";
 
